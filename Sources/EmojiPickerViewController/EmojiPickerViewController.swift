939c0fa822bb992cf59f7407c431abcd37fb7814
yosshi4486 <yoshiharuseijin@gmail.com>
2022-01-27 13:38:16 +0900

    Create a package.

---
+++ Sources/EmojiPickerViewController/EmojiPickerViewController.swift
@@ -0,0 +1,6 @@
+public struct EmojiPickerViewController {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/EmojiPickerViewControllerTests/EmojiPickerViewControllerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EmojiPickerViewController
+
+final class EmojiPickerViewControllerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EmojiPickerViewController().text, "Hello, World!")
+    }
+}
