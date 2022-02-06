5f18e784015c7d750937e10056a60686ab1cbbb0
yosshi4486 <yoshiharuseijin@gmail.com>
2022-02-06 10:54:38 +0900

    Fix example to move setupper to viewDidAppear

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
         let emojiPicker = EmojiPickerViewController()
         let navigationController = UINavigationController(rootViewController: emojiPicker)
