4e74c9c554acccc5183495c205e3ca76e659153e
yosshi4486 <yoshiharuseijin@gmail.com>
2022-02-06 17:46:30 +0900

    Refine example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         resultLabel.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            resultLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            resultLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             resultLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             resultLabel.heightAnchor.constraint(equalToConstant: 50)
@@ -27,10 +27,13 @@ class ViewController: UIViewController {
 
         resultLabel.text = ""
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Pick Emoji", image: nil, primaryAction: UIAction(handler: { [unowned self] _ in
+            self.showPicker()
+        }))
+
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    func showPicker() {
 
         let emojiPicker = EmojiPickerViewController()
         emojiPicker.delegate = self
@@ -39,17 +42,34 @@ class ViewController: UIViewController {
          You can specify a language for searching and voiceover.
          */
         emojiPicker.emojiContainer.annotationLocale = .init(languageIdentifier: "ja")!
-        
-        let navigationController = UINavigationController(rootViewController: emojiPicker)
-        navigationController.modalPresentationStyle = .pageSheet
 
-        let sheet = navigationController.sheetPresentationController!
-        sheet.delegate = self
-        sheet.detents = [.medium(), .large()]
+        let navigationController = UINavigationController(rootViewController: emojiPicker)
+
+        if traitCollection.userInterfaceIdiom == .pad {
+
+            navigationController.modalPresentationStyle = .popover
+
+            let popover = navigationController.popoverPresentationController!
+            popover.barButtonItem = navigationItem.rightBarButtonItem
+            popover.delegate = self
+
+            let sheet = popover.adaptiveSheetPresentationController
+            sheet.delegate = self
+            sheet.detents = [.medium(), .large()]
+
+        } else {
+
+            navigationController.modalPresentationStyle = .pageSheet
+
+            let sheet = navigationController.sheetPresentationController!
+            sheet.delegate = self
+            sheet.detents = [.medium(), .large()]
+
+        }
 
         present(navigationController, animated: true)
-    }
 
+    }
 
 }
 
@@ -58,6 +78,12 @@ extension ViewController: EmojiPickerViewControllerDelegate {
     func emojiPickerViewController(_ emojiPickerViewController: EmojiPickerViewController, didPick emoji: Emoji) {
         print("The user picked \(emoji)")
         resultLabel.text!.append(emoji.character)
+
+        /*
+         Dismiss the `emojiPickerViewController` here if you don't want to allow multiple picking.
+         */
+
+        // emojiPickerViewController.dismiss(animated: true, completion: nil)
     }
 
     func emojiPickerViewController(_ emojiPickerViewController: EmojiPickerViewController, didReceiveError error: Error) {
@@ -71,5 +97,9 @@ extension ViewController: UISheetPresentationControllerDelegate {
     func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
         print("EmojiPickerViewController did dismiss.")
     }
+
+}
+
+extension ViewController: UIPopoverPresentationControllerDelegate {
 
 }
