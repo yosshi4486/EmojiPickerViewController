d7bd1bfecf39afd84a81dc1a9755fc85de4b7dea
yosshi4486 <yoshiharuseijin@gmail.com>
2022-02-06 10:46:24 +0900

    Add the package as local

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import EmojiPickerViewController
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let emojiPicker = EmojiPickerViewController()
+        let navigationController = UINavigationController(rootViewController: emojiPicker)
+        navigationController.modalPresentationStyle = .pageSheet
+
+        let sheet = navigationController.sheetPresentationController!
+        sheet.detents = [.medium(), .large()]
+
+        present(navigationController, animated: true)
     }
 
 
